562487be04dfa552feadee60c3c89fc85b4096e4
blade <blade8128ch@gmail.com>
2023-07-11 00:09:08 +0800

    refactor: modify mongoose config and create router

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-// Include express from node_modules
 const express = require('express')
-//const mongoose = require('mongoose') // 載入 mongoose
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
-/*
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}) // 設定連線到 mongoDB
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-*/
-// Define server related variables
 const port = 3000
 
 const exphbs = require('express-handlebars')
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,7 +5,6 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 // 定義首頁路由
 router.get('/', (req, res) => {
-  //res.render("index", { restaurants: restaurantList.results })
   Restaurant.find() // 取出  model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
