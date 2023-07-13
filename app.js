1cb26562d9adc25807a7471344c94e21b2fc2fa1
blade <blade8128ch@gmail.com>
2023-07-13 23:25:55 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -31,11 +32,13 @@ app.use(express.static('public'))
 
 app.use(methodOverride('_method'))
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg') // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg') // 設定 warning_msg 訊息
   next()
 })
 
