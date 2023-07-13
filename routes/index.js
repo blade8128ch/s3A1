b0dd5c9571869c46cf21f133a45f4b9d0073f3e8
Blade <blade8128ch@gmail.com>
2023-07-13 09:49:18 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 // 準備引入路由模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 // 匯出路由器
 module.exports = router
